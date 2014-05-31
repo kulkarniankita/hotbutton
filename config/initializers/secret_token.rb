9d91b0e9a76ba5df6aee209e22300f47d9b7ccd2
Naomi <naomi.freeman@yahoo.ca>
2014-05-31 17:39:48 -0400

    base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hotbutton::Application.config.secret_key_base = 'a2fc1e198096b415945c5c6a16669ef5d5c12b056ed9950a4de56fe0d3cc0552cb371ab0b75b4ace8f97686698b4c0423e3960d0ba273b562c57ab9a5dfba4af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hotbutton::Application.config.session_store :cookie_store, key: '_hotbutton_session'
